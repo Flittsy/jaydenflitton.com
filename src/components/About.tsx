1c78ca12b3100e256a918f55362b7461c3450cf9
Flittsy <jayden.flitton@outlook.com>
2026-06-02 11:15:28 -0600

    update about.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       id="about"
       eyebrow="Background"
       title="About"
-      description="I'm Jayden Flitton, an engineering and finance student building technical projects and developing business thinking at the intersection of hardware, software, and markets."
+      description="My name is Jayden Flitton, I like to understand our reality, and use that understanding to generate value. My growing sphere of knowledge includes general engineering, financial strategy, agriculture, and software development. I am a student taking a dual degree in electrical engineering and finance at the University of Calgary. I hope you can derive some value from my projects on this page."
     />
   );
 }
